520059aa590ff3c8bd2f65df225275b6cd04a4ea
Marcus Furlong <furlongm@gmail.com>
2014-05-16 17:33:44 +1000

    trusty support for fail2ban
    Change-Id: Ie6d6179711757f4e12c85ec0fd243e169d6ed448

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,25 +5,55 @@ class fail2ban {
     notify => Service['fail2ban'],
   }
 
+  case $::lsbdistcodename {
+    precise: { $fail2banconf='fail2ban.conf' $jailconf='jail.conf' }
+    trusty:  { $fail2banconf='fail2ban.local' $jailconf='jail.local' }
+    default: { fail("Unknown lsbdistcodename : '$::{lsbdistcodename}'") }
+  }
+
+  file { ['/etc/fail2ban/fail2ban.conf.local','/etc/fail2ban/jail.conf.local']:
+    ensure => absent,
+  }
+
   service { 'fail2ban':
     ensure    => running,
     subscribe => Package['fail2ban'],
-    require   => File['/etc/fail2ban/jail.conf', '/etc/fail2ban/fail2ban.conf'],
+    require   => File["/etc/fail2ban/$jailconf", "/etc/fail2ban/$fail2banconf"],
   }
 
-  file { '/etc/fail2ban/jail.conf':
-    source  => 'puppet:///modules/fail2ban/jail.conf',
+  file { "/etc/fail2ban/$jailconf":
+    source  => "puppet:///modules/fail2ban/$jailconf",
     owner   => root,
     group   => root,
     mode    => '0644',
     require => Package['fail2ban'],
   }
 
-  file { '/etc/fail2ban/fail2ban.conf':
-    source  => 'puppet:///modules/fail2ban/fail2ban.conf',
+  file { "/etc/fail2ban/$fail2banconf":
+    source  => "puppet:///modules/fail2ban/$fail2banconf",
     owner   => root,
     group   => root,
     mode    => '0644',
     require => Package['fail2ban'],
+  }
+
+  # This can be removed once we have run this on all hosts once
+  # It just restores the original distro file for trusty
+  if $::lsbdistcodename == 'trusty' {
+    file { "/etc/fail2ban/jail.conf":
+      source  => "puppet:///modules/fail2ban/jail.conf-trusty",
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      require => Package['fail2ban'],
+    }
+
+    file { "/etc/fail2ban/fail2ban.conf":
+      source  => "puppet:///modules/fail2ban/fail2ban.conf-trusty",
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      require => Package['fail2ban'],
+    }
   }
 }

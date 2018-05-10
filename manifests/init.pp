cb51da103d5f93babcd06703c111008eb83a40dd
Jake Yip <jake.yip@unimelb.edu.au>
2018-05-10 11:54:36 +1000

    Add bionic support
    And remove old code handling trusty and precise
    
    Change-Id: Ia66694cdbe34616f78f078d0f504a3a330afbbeb

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,4 @@
+# Configures fail2ban
 class fail2ban {
 
   package { 'fail2ban':
@@ -5,56 +6,25 @@ class fail2ban {
     notify => Service['fail2ban'],
   }
 
-  case $::lsbdistcodename {
-    'precise': { $fail2banconf='fail2ban.conf' $jailconf='jail.conf' }
-    'trusty':  { $fail2banconf='fail2ban.local' $jailconf='jail.local' }
-    'xenial':  { $fail2banconf='fail2ban.local' $jailconf='jail.local' }
-    default: { fail("Unknown lsbdistcodename : '$::{lsbdistcodename}'") }
-  }
-
-  file { ['/etc/fail2ban/fail2ban.conf.local','/etc/fail2ban/jail.conf.local']:
-    ensure => absent,
-  }
-
   service { 'fail2ban':
     ensure    => running,
     subscribe => Package['fail2ban'],
-    require   => File["/etc/fail2ban/$jailconf", "/etc/fail2ban/$fail2banconf"],
+    require   => File['/etc/fail2ban/jail.local', '/etc/fail2ban/fail2ban.local'],
   }
 
-  file { "/etc/fail2ban/$jailconf":
-    source  => "puppet:///modules/fail2ban/$jailconf",
+  file { '/etc/fail2ban/jail.local':
+    source  => 'puppet:///modules/fail2ban/jail.local',
     owner   => root,
     group   => root,
     mode    => '0644',
     require => Package['fail2ban'],
   }
 
-  file { "/etc/fail2ban/$fail2banconf":
-    source  => "puppet:///modules/fail2ban/$fail2banconf",
+  file { '/etc/fail2ban/fail2ban.local':
+    source  => 'puppet:///modules/fail2ban/fail2ban.local',
     owner   => root,
     group   => root,
     mode    => '0644',
     require => Package['fail2ban'],
-  }
-
-  # This can be removed once we have run this on all hosts once
-  # It just restores the original distro file for trusty
-  if $::lsbdistcodename == 'trusty' {
-    file { "/etc/fail2ban/jail.conf":
-      source  => "puppet:///modules/fail2ban/jail.conf-trusty",
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      require => Package['fail2ban'],
-    }
-
-    file { "/etc/fail2ban/fail2ban.conf":
-      source  => "puppet:///modules/fail2ban/fail2ban.conf-trusty",
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      require => Package['fail2ban'],
-    }
   }
 }
